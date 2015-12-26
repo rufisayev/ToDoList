52b0b6c1bb452f11adb59f93da4ef394dc351890
rufisayev <rufetisayev@yahoo.com>
2015-12-26 22:51:02 +0400

    Selected and unselected first edition done

--- src/main-page.js
+++ src/main-page.js
@@ -4,6 +4,8 @@ var ReactFire = require('reactfire');
 var FireBase = require('firebase');
 var fireBaseUrl = 'https://todolistprject.firebaseio.com/';
 var ListPanel = require('./list-panel');
+var update = require('react-addons-update');
+var _ = require('underscore');
 
 var MainPage = React.createClass({
 
@@ -26,6 +28,18 @@ var MainPage = React.createClass({
                         className="btn btn-default">
                         Delete all
                     </button>
+                    <button
+                        type="button"
+                        onClick={this.handleSelectedClick}
+                        className="btn btn-default">
+                        Selected
+                    </button>
+                    <button
+                        type="button"
+                        onClick={this.handleUnSelectedClick}
+                        className="btn btn-default">
+                        UnSelected
+                    </button>
                 </div>
             </div>
         </div>
@@ -36,21 +50,57 @@ var MainPage = React.createClass({
     getInitialState: function () {
         return {
             toDoList: {},
-            dataLoaded: false
+            dataLoaded: false,
+            stateChanged: false,
+            dummyData: []
         }
     },
+
     componentWillMount: function () {
         this.fireBase = new FireBase(fireBaseUrl + 'toDoList');
         this.bindAsObject(this.fireBase, 'toDoList');
         this.fireBase.on('value', this.handleToDoListLoaded);
     },
-    handleDeleteAllClick:function(event){
-        for(var key in this.state.toDoList){
-            if(this.state.toDoList[key].isFinished === true){
+    componentDidMount: function () {
+        console.log('todo', this.state.toDoList)
+    },
+    handleSelectedClick: function (event) {
+        if (this.state.stateChanged === false) {
+            console.log(1)
+            this.setState({stateChanged: true, dummyData: this.state.toDoList})
+        } else {
+            this.setState({toDoList: this.state.dummyData});
+        }
+
+        var result = _.filter(this.state.toDoList, function (item) {
+            return item.isFinished === true;
+        });
+        this.setState({toDoList: result})
+    },
+    handleUnSelectedClick: function (event) {
+        if (this.state.stateChanged === false) {
+            this.setState({stateChanged: true, dummyData: this.state.toDoList})
+        } else {
+            console.log('dummy', this.state.dummyData)
+            console.log('before', this.state.toDoList);
+            this.setState({toDoList: this.state.dummyData});
+        }
+
+        console.log('after', this.state.toDoList);
+        var result = _.filter(this.state.toDoList, function (item) {
+            return item.isFinished === false;
+        });
+        this.setState({toDoList: result});
+    },
+
+    handleDeleteAllClick: function (event) {
+        for (var key in this.state.toDoList) {
+            if (this.state.toDoList[key].isFinished === true) {
                 this.fireBase.child(key).remove();
             }
         }
     },
+
     handleToDoListLoaded: function () {
         this.setState({dataLoaded: true});
     }
